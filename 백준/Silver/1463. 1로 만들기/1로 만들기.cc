81d151cb22667cb8a1451bdf0b822adc08299795
Seok_H_6704 <124482528+Seok6704@users.noreply.github.com>
2024-08-15 13:27:15 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> D;
+    D.resize(N + 1);
+
+    D[0] = 0;
+    D[1] = 0;
+
+    for(int i = 2; i <= N; i++)
+    {
+        D[i] = D[i - 1] + 1;
+        if(i % 2 == 0)
+        {
+            D[i] = min(D[i], D[i / 2] + 1);
+        }
+        if(i % 3 == 0)
+        {
+            D[i] = min(D[i], D[i / 3] + 1);
+        }
+    }
+
+    cout << D[N] << "\n";
+
+}
