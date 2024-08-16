0e60da309fcc8cebcf1f3b46fa5207b56d589303
Seok_H_6704 <124482528+Seok6704@users.noreply.github.com>
2024-08-16 21:14:51 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -3,62 +3,59 @@
 
 using namespace std;
 
+static vector<int> U;
 void Union(int a, int b);
 int Find(int a);
-void CheckSame(int a, int b);
-static vector<int> s;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    
     int n, m;
     cin >> n >> m;
 
-
     for(int i = 0; i <= n; i++)
     {
-        s.push_back(i);
+        U.push_back(i);
     }
 
     for(int i = 0; i < m; i++)
     {
-        int c, a, b;
-        cin >> c >> a >> b;
+        int a, b, c;
+        cin >> a >> b >> c;
 
-        if(c == 0)
+        if(a == 0)
         {
-            Union(a, b);
+            Union(b, c);
         }
         else
-        {   
-            CheckSame(a, b);
+        {
+            int rb = Find(b);
+            int rc = Find(c);
+            if(rb == rc) cout << "YES" << "\n";
+            else cout << "NO" << "\n";
         }
     }
-
 }
 
 void Union(int a, int b)
 {
     a = Find(a);
     b = Find(b);
-
-    if(a != b) s[b] = a;
+    
+    if(a != b) U[b] = a;
 }
 
 int Find(int a)
 {
-    if(a == s[a]) return a;
+    if(a == U[a])
+    {
+        return a;
+    }
     else
-        return s[a] = Find(s[a]);
-}
-
-void CheckSame(int a, int b)
-{
-    a = Find(a);
-    b = Find(b);
-
-    if(a == b) cout << "YES" << "\n";
-    else cout << "NO" << "\n";
+    {
+        return U[a] = Find(U[a]);
+    }
 }
